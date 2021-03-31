fa6c6a32bef041f4c415f19baaaaeb7cdca832e7
bahrian2k18 <40366924+bahrian2k18@users.noreply.github.com>
2021-03-31 18:09:53 +0500

    third exercise covering useState

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App'
-import App from './App';
-import './index.css';
+import react, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
 
-function Movies(props)
+function Room()
 {
- return <div> 
-     
-    <App/>
+   
+    const state = {
+        isLit: true,
+        temperature: 22
+        
+    }
 
-      </div>
-} ReactDOM.render(<App/>, document.querySelector('#root'));
+    var flipLight = () => {
+        setState({isLit: !state.isLit})
+      }
+
+     var turnOn = () => {
+        state.isLit ? null: this.setState({isLit: !state.isLit});
+      }
+
+     var turnOff = () => {
+        state.isLit ? setState({isLit: !state.isLit}): null;
+      }
+     var tempInc = () => {
+        this.setState({
+          temperature: this.state.temperature + 1
+        });
+      };
+      var tempDec = () => {
+        this.setState({
+          temperature: this.state.temperature - 1
+        });
+      };
+
+    const[isLit, temperature] = this.state;
+    const brightness = isLit ? 'lit' : 'dark';
+
+return(
+<div className={`room ${brightness}`}>
+
+this room is {isLit ? 'lit' : 'dark'}
+
+<button onClick={()=>setLit(!isLit)}>
+    flip
+</button>
+
+<button onClick={turnOn} >ON</button>
+
+<button onClick={turnOff}>OFF</button>
+<p>Room temperature is: {temperature}</p>
+<br/>
+<button onClick={tempInc}>+</button>
+<button onClick={tempDec}>-</button>
+
+</div>
+);
+}
+ReactDOM.render(<Room/>, document.getElementById('root'));
